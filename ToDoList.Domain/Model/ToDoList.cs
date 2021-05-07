2821d9152d3d9a5de5a2a2f256b6cc34b4cb3258
Krzysztof Wardziak <krzysztof.wardziak@oke.pl>
2021-05-07 09:57:42 +0200

    Add project files.

---
+++ ToDoList.Domain/Model/ToDoList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList.Domain.Model
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+        public string DateTime { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
